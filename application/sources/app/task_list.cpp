4c34673175df9d4b4e17d284c299ceac25464ea5
Đinh Quốc Cường <tr6r20@gmail.com>
2025-11-03 14:36:17 +0700

    DEL shell.cpp

--- application/sources/app/task_list.cpp
+++ application/sources/app/task_list.cpp
@@ -6,7 +6,6 @@ const task_t app_task_table[] = {
 	/* SYSTEM TASK */
 	/*************************************************************************/
 	{TASK_TIMER_TICK_ID,		TASK_PRI_LEVEL_7,		task_timer_tick			},
-
 	/*************************************************************************/
 	/* APP TASK */
 	/*************************************************************************/
@@ -14,24 +13,6 @@ const task_t app_task_table[] = {
 	{AC_TASK_LIFE_ID			,	TASK_PRI_LEVEL_6	,	task_life			},
 	{AC_TASK_DBG_ID				,	TASK_PRI_LEVEL_4	,	task_dbg			},
 	{AC_TASK_DISPLAY_ID			,	TASK_PRI_LEVEL_4	,	task_display		},
-
-	/*************************************************************************/
-	/* RF24 NETWORK TASK */
-	/*************************************************************************/
-	#if defined (IF_NETWORK_NRF24_EN)
-	{AC_RF24_PHY_ID				,	TASK_PRI_LEVEL_3	,	task_rf24_phy		},
-	{AC_RF24_MAC_ID				,	TASK_PRI_LEVEL_4	,	task_rf24_mac		},
-	{AC_RF24_NWK_ID				,	TASK_PRI_LEVEL_5	,	task_rf24_nwk		},
-	#endif
-
-	/*************************************************************************/
-	/* LINK TASK */
-	/*************************************************************************/
-	#if defined (IF_LINK_UART_EN)
-	{AC_LINK_PHY_ID				,	TASK_PRI_LEVEL_3	,	task_link_phy		},
-	{AC_LINK_MAC_ID				,	TASK_PRI_LEVEL_4	,	task_link_mac		},
-	{AC_LINK_ID					,	TASK_PRI_LEVEL_5	,	task_link			},
-	#endif
 
 	/*************************************************************************/
 	/* END OF TABLE */
